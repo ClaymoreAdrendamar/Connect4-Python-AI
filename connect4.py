8ef9084427695911a8d2f58b0dc5a362e04ebc45
Joseph Chotard <joseph@gamepickle.net>
2017-01-31 10:43:15 +0100

    Add files via upload

---
+++ connect4.py
@@ -0,0 +1,165 @@
+#  0 1 2 3 4 5 6
+#0 0 0 0 0 0 0 0
+#1 0 0 0 0 0 0 0
+#2 0 0 0 0 0 0 0
+#3 0 0 0 0 0 0 0
+#4 0 0 0 0 0 0 0
+#5 0 0 0 0 0 0 0
+
+# - A Classic 7*6 Board -
+# A board contains 42 Squares
+# Each Square can:
+#    - [ ] Be empty 
+#    - [O] Contain a white Man
+#    - [X] Contain a black Man
+
+# A Square is characterized by its column and its row
+# [column][row]
+
+from environment import *
+import random
+from copy import deepcopy
+
+COMPUTER = 'O'
+
+def player_move(board, text):
+    """ Get a player's move"""
+    print('\n-------')
+    print(text)
+    move = None
+    while move not in board.legal_moves():
+        move = int(input('Select square to play at: '))-1
+    return move
+
+def computer_move(board, computer):
+    """ Get the stupod computer's move """
+    board = deepcopy(board) # Copy board as we will be changing it
+    best_moves = [0,1,2,3,4,5,6]
+    player = get_opponent(computer)
+
+    # If computer can win: play there
+    for move in board.legal_moves():
+        board.move(move, computer)
+        if board.winner() == computer:
+            print('Computer will play at square: {}'.format(move+1))
+            return move
+        # Undo the move
+        board.move(move, ' ', True)
+        
+    # If player can win: block the move
+    for move in board.legal_moves():
+        board.move(move, player)
+        if board.winner() == player:
+            print('Computer will block at square: {}'.format(move+1))
+            return move
+        # Undo the move
+        board.move(move, ' ', True)
+
+    # Else pick the best empty square
+    for move in best_moves:
+        if move in board.legal_moves():
+            print('Computer will default to square: {}'.format(move+1))
+            return move
+
+# Minimax
+def minimax(node, player, alpha, beta, depth):
+    if node.leaf() or depth == 0:
+        return node.evaluate('O')
+
+    for move in node.legal_moves():
+        node.move(move, player)
+        score = minimax(node, get_opponent(player), alpha, beta, depth-1)
+        node.move(move, ' ', True)
+        if player == 'O':
+            if score > alpha:
+                alpha = score
+            if alpha >= beta:
+                return beta
+        else:
+            if score < beta:
+                beta = score
+            if beta <= alpha:
+                return alpha
+    if player == 'O':
+        return alpha
+    else:
+        return beta
+
+def computer_move2(board, player):
+    best = LOSE-1
+    choices = []
+    
+    if len(board.legal_moves()) == 9:
+        return random.choice([0,2,6,8])
+    for move in board.legal_moves():
+        board.move(move, player)
+        score = minimax(board, get_opponent(player), LOSE*2, WIN*2, 6)
+        board.move(move, ' ', True)
+        print("Move: ",move+1," has a score of: ",score)
+        if score > best:
+            best = score
+            choices = [move]
+        elif score == best:
+            choices.append(move)
+    choice = random.choice(choices)
+    print("[+] Selected move: ",choice+1)
+    return choice
+
+def play():
+    """ Play Tic-Tac-Toe"""
+    board = Board()
+    print(board)
+    player1 = input("What is X called? ")
+    player2 = input("What is O called? ")
+    print('{} is X and {} is O'.format(player1,player2))
+    player = None
+    while player not in ['X','O']:
+         player = input('Who goes first(X or O)? ').capitalize()
+
+    while not board.leaf():
+        if player == 'X':
+            move = player_move(board, '{}\'s turn'.format(player1))
+            board.move(move, player)
+        else:
+            move = player_move(board, '{}\'s turn'.format(player2))
+            board.move(move, player)
+        print(board)
+        player = get_opponent(player)
+
+    if board.X_won():
+        print("{} won!".format(player1))
+    elif board.O_won():
+        print("{} won!".format(player2))
+    else:
+        print("It's a tie!")
+        
+def computer_play():
+    """ Play Tic-Tac-Toe against a (perfect) computer"""
+    board = Board()
+    print(board)
+    player1 = input("What are you called? ")
+    player2 = "Computer"
+    print('{} is X and {} is O'.format(player1,player2))
+    player = None
+    while player not in ['X','O']:
+         player = input('Who goes first?(X or O) ').capitalize()
+
+    while not board.leaf():
+        if player == 'X':
+            move = player_move(board, '{}\'s turn'.format(player1))
+            board.move(move, player)
+        else:
+            move = computer_move2(board, player)
+            board.move(move, player)
+        print(board)
+        player = get_opponent(player)
+
+    if board.X_won():
+        print("{} won!".format(player1))
+    elif board.O_won():
+        print("{} won!".format(player2))
+    else:
+        print("It's a tie!")
+        
+if __name__ == '__main__':
+    computer_play()
---
+++ environment.py
@@ -0,0 +1,183 @@
+WIN  =  512
+LOSE =  -WIN
+
+#self.board environment class
+class Board(object):
+    """ Game Environment Class"""
+    
+    def __init__(self, board=[]):
+        """ Initialize the environment variables """
+        # Generate a newself.board if the passedself.board is empty
+        if len(board) == 0:
+            self.board = [[' ' for row in range(6)] for column in range(7)]
+
+        # Set newself.board as oldself.board
+        else:
+            self.board = self.board
+
+        self.width = 7
+        self.height = 6
+
+    def __str__(self):
+        """ Return the board"""
+        string = ''
+        # Print Column numbers
+        string += "\ 1 2 3 4 5 6 7\n"
+
+        # 0-5 (6 Rows)
+        for row in range(6):
+            # Print Row number
+            string += str(row+1) + ' '
+            # 0-6 (7 Columns)
+            for column in range(7):
+                # Print Square at index [Column][Row]
+                string += self.board[column][row] + ' '
+            # New line
+            string += '\n'
+        return string
+
+    def legal_moves(self):
+        """ Get the empty spaces """
+        return self.get_columns(' ')
+
+    def leaf(self):
+        """ Is theself.board full or has someone won the game """
+        if self.full():
+            return True
+        if self.winner() != None:
+            return True
+        return False
+
+    def full(self):
+        for column in range(self.width):
+            if self.board[column][0] == ' ':
+                return False
+        return True
+
+    def X_won(self):
+        """ Did player X win """
+        return self.winner() == 'X'
+
+    def O_won(self):
+        """ Did player O win """
+        return self.winner() == 'O'
+
+    def tied(self):
+        """ Is the game a tie? """
+        return self.leaf() == True and self.winner() is None
+
+    def score(self, one, two, three, four, man):
+        o_man = get_opponent(man)
+        score = 0
+        if(one != o_man and two != o_man and three != o_man and four != o_man):
+                    count = 0
+                    if one == man:
+                        count += 1
+                    if two == man:
+                        count += 1
+                    if three == man:
+                        count += 1
+                    if four == man:
+                        count += 1
+                  
+                    if count == 4:
+                        score += WIN
+                    elif count == 3:
+                        score += 50
+                    elif count == 2:
+                        score += 10
+                    elif count == 1:
+                        score += 1
+        return score
+
+    def evaluate(self, man):
+        o_man = get_opponent(man)
+        score = 0
+        for column in range(self.width):
+            for row in range(self.height):
+                # Horizonatal - evaluation
+                try:
+                    t_score = self.score(self.board[column][row], self.board[column+1][row], self.board[column+2][row], self.board[column+3][row], man)
+                    if t_score >= WIN:
+                        return WIN
+                    score += t_score
+                    t_score = self.score(self.board[column][row], self.board[column+1][row], self.board[column+2][row], self.board[column+3][row], o_man)
+                    if t_score >= WIN:
+                        return LOSE
+                    score -= t_score
+                except:
+                    pass
+                # Vertical | evaluation
+                try:
+                    t_score = self.score(self.board[column][row], self.board[column][row+1], self.board[column][row+2], self.board[column][row+3], man)
+                    if t_score >= WIN:
+                        return WIN
+                    score += t_score
+                    t_score = self.score(self.board[column][row], self.board[column][row+1], self.board[column][row+2], self.board[column][row+3], o_man)
+                    if t_score >= WIN:
+                        return LOSE
+                    score -= t_score
+                except:
+                    pass
+                # Diagonal \ evaluation
+                try:
+                    t_score = self.score(self.board[column][row], self.board[column+1][row+1], self.board[column+2][row+2], self.board[column+3][row+3], man)
+                    if t_score >= WIN:
+                        return WIN
+                    score += t_score
+                    t_score = self.score(self.board[column][row], self.board[column+1][row+1], self.board[column+2][row+2], self.board[column+3][row+3], o_man)
+                    if t_score >= WIN:
+                        return LOSE
+                    score -= t_score
+
+                except:
+                    pass
+                # Diagonal / evaluation
+                try:
+                    t_score = self.score(self.board[column][row], self.board[column-1][row+1], self.board[column-2][row+2], self.board[column-3][row+3], man)
+                    if t_score >= WIN:
+                        return WIN
+                    score += t_score
+                    t_score = self.score(self.board[column][row], self.board[column-1][row+1], self.board[column-2][row+2], self.board[column-3][row+3], o_man)
+                    if t_score >= WIN:
+                        return LOSE
+                    score -= t_score
+                except:
+                    pass
+        if man == 'O':
+            score += 16
+        else:
+            score -= 16
+        return score
+                
+    def winner(self):
+        """ Get the winner of the board """
+        if self.evaluate('X') >= WIN:
+            return 'X'
+        if self.evaluate('O') >= WIN:
+            return 'O'
+        return None
+
+    def get_columns(self, player):
+        """ Get a list of all squares taken by a certain player """
+        return [column for column in range(self.width) if self.board[column][0] == player]
+
+    def move(self, column, player, delete=False):
+        """ Move player to position """
+        # Get Row to play at
+        for row in range(self.height):
+            if self.board[column][row] != ' ':
+                if not(delete):
+                    row -= 1
+                break
+
+        # Set Square at index [column-1][row-1] to passed Square value
+        #print("Column: ",column+1," Row: ",row+1)
+        self.board[column][row] = player
+
+def get_opponent(player):
+    """ Gives us the opponent of player """
+    if player == 'O':
+        return 'X'
+    else:
+        return 'O'
